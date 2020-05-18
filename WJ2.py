da2e36bfa4fd6107dddd17bdc3dcec7348e49684
WJbayway <65524778+WJbayway@users.noreply.github.com>
2020-05-18 19:52:03 +0900

    5월 18일 수정

---
+++ WJ2.py
@@ -0,0 +1,17 @@
+import WJ1
+from WJ3 import add, sub
+
+print(__name__)
+
+
+result = add(5,6)
+print(result)
+
+hyori = WJ1.Woman(45,165,120)
+
+hyori.show()
+hyori.eat()
+hyori.sleep()
+hyori.study()
+
+hyori.show()
---
+++ WJ3.py
@@ -0,0 +1,7 @@
+print(__name__)
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
